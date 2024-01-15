e2e34ef5d04a2fc38eb158583244d2b757fc3154
Alessandro da Silva Oliveira - AOl <alessandro.oliveira@iteris.com.br>
2024-01-15 20:35:05 -0300

    configurando múltiplas execuções de job e obtenção de parâmetros

--- src/main/java/br/com/primeiroprojetospringbatch/BatchConfig.java
+++ src/main/java/br/com/primeiroprojetospringbatch/BatchConfig.java
@@ -6,10 +6,12 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,13 +24,16 @@ public class BatchConfig {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    @Value("${nome.parameter}")
+    String nome;
+
     private Step imprimeOlaStep() {
         return stepBuilderFactory
                 .get("imprimeOlaStep")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Olá, mundo!");
+                        System.out.printf("Olá, %s", nome);
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
@@ -40,6 +45,7 @@ public class BatchConfig {
         return jobBuilderFactory
                 .get("imprimeOlaJob")
                 .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 }
